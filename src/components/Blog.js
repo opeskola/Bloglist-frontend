506f444b95c873fe7c5e43b87cef655e7766cd3a
opeskola <opeskola@gmail.com>
2019-11-01 16:52:17 +0200

    5.6 done

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,35 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const blogStyle = {
+  paddingTop: 10,
+  paddingLeft: 2,
+  border: 'solid',
+  borderWidth: 1,
+  marginBottom: 5
+}
+
+const Blog = ({ blog }) => {
+
+  const [blogInfo, setBlogInfo] = useState(false)
+  const blogInfoVisibility = () => {setBlogInfo(!blogInfo)}
+
+  return (
+    <div style={blogStyle}>
+      <div onClick={(blogInfoVisibility)}>
+        {blogInfo === false ?
+          <div>{blog.title} {blog.author}</div> :
+          <div>
+            <div>{blog.title} {blog.author}</div> 
+            <div>{blog.url}</div>
+            <div>{blog.likes} likes 
+              <button>like</button>
+            </div>
+            <div>added by {blog.user.name}</div>
+          </div>
+        }     
+      </div>
+    </div>
+  )
+}
 
 export default Blog
